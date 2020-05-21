73a56adac35ce4d1e6a105d8b463cab107646125
Rishabh Kumar Verma <35322947+sawyer1997@users.noreply.github.com>
2020-05-21 10:57:37 +0530

    Update constants.py

--- candidates/constants.py
+++ candidates/constants.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 END_TIME_STAMP = datetime.timestamp(datetime(2020, 5, 30))
 API_ENDPOINT = "http://127.0.0.1:3000"
-SENDER_MAIL_ID = "rishabhverma220898@gmail.com"
+SENDER_MAIL_ID = ""
